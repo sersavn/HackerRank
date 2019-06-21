0021d77d801182c152d64c22695b84da46e9a299
sersavn <nooooooooob@yandex.ru>
2019-06-22 00:23:57 +0300

    pylint fix

--- Tutorials/10_days_of_statistics/day_0_mean_median_mode.py
+++ Tutorials/10_days_of_statistics/day_0_mean_median_mode.py
@@ -12,7 +12,8 @@ Print  lines of output in the following order:
 
 Print the mean on a new line, to a scale of 1 decimal place (i.e., 12.3 ,7.0).
 Print the median on a new line, to a scale of 1 decimal place (i.e., 12.3 ,7.0).
-Print the mode on a new line; if more than one such value exists, print the numerically smallest one.
+Print the mode on a new line;
+if more than one such value exists, print the numerically smallest one.
 '''
 
 '''
@@ -31,43 +32,42 @@ Sample Output
 '''
 
 import fileinput
-import sys
 
-input_str = []
+INPUTSTR = []
 for n in fileinput.input():
-    input_str.append(n.split(" "))
+    INPUTSTR.append(n.split(" "))
 
-num_amount = int(input_str[0][0])
-nums = input_str[1:][0]
-nums = sorted([int(i) for i in nums])
+NUM_AMOUNT = int(INPUTSTR[0][0])
+NUMS = INPUTSTR[1:][0]
+NUMS = sorted([int(i) for i in NUMS])
 
 def mean():
-    num_sum = sum(nums)
-    return num_sum/num_amount
+    num_sum = sum(NUMS)
+    return num_sum/NUM_AMOUNT
 
 def median():
-    if num_amount%2==0:
-        median_id = int(num_amount/2)
-        return (nums[median_id-1] + nums[median_id])/2
+    if NUM_AMOUNT%2 == 0:
+        median_id = int(NUM_AMOUNT/2)
+        return (NUMS[median_id-1] + NUMS[median_id])/2
     else:
-        return nums(int(num_amount/2))
+        return NUMS(int(NUM_AMOUNT/2))
 
 def mode():
     vals_dict = dict()
-    for i in set(nums):
+    for i in set(NUMS):
         vals_dict[i] = 0
 
-    for i in nums:
+    for i in NUMS:
         vals_dict[i] += 1
 
     highest_v_candidates = []
-    for k,v in vals_dict.items():
+    for k, v in vals_dict.items():
         highest_v_candidates.append(v)
 
     highest_v = max(highest_v_candidates)
 
     potential_mode = []
-    for k,v in vals_dict.items():
+    for k, v in vals_dict.items():
         if highest_v == v:
             potential_mode.append(k)
     return min(potential_mode)
